b4af8742e5d11712f5b4c0abd46e7d6e853a3b15
Cristi DeLeo <cristi.deleo@asu.edu>
2020-02-09 17:40:42 -0500

    [02/09/2020] Basic database implementation with Insert/Update/Delete/Index/Filter functions

--- app.js
+++ app.js
@@ -7,17 +7,119 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var mongoose = require('mongoose');
+// BEGIN MONGOOSE DB INITIALIZATION
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
-//Set up default mongoose connection
-var mongoDB = 'mongodb://127.0.0.1/my_database';
-mongoose.connect(mongoDB, { useNewUrlParser: true });
+// Connection URL
+const url = 'mongodb://localhost:27017';
 
-//Get the default connection
-var db = mongoose.connection;
+// Database Name
+const dbName = 'ser416';
 
-//Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+const insertDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('documents');
+  // Insert some documents
+  collection.insertMany([
+    {a : 1}, {a : 2}, {a : 3}
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
+};
+
+const findDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('documents');
+  // Find some documents
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(docs)
+    callback(docs);
+  });
+};
+
+// Find Documents with a Query Filter
+// Add a query filter to find only documents which meet the query criteria.
+const findFilteredDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('documents');
+  // Find some documents
+  collection.find({'a': 3}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(docs);
+    callback(docs);
+  });
+}; // Only the documents which match ``'a' : 3`` should be returned.
+
+// Update a document
+// The following operation updates a document in the **documents** collection.
+const updateDocument = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('documents');
+  // Update document where a is 2, set b equal to 1
+  collection.updateOne({ a : 2 }
+    , { $set: { b : 1 } }, function(err, result) {
+    assert.equal(err, null);
+    assert.equal(1, result.result.n);
+    console.log("Updated the document with the field a equal to 2");
+    callback(result);
+  });
+};
+
+// Remove a document
+// Remove the document where the field **a** is equal to **3**.
+const removeDocument = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('documents');
+  // Delete document where a is 3
+  collection.deleteOne({ a : 3 }, function(err, result) {
+    assert.equal(err, null);
+    assert.equal(1, result.result.n);
+    console.log("Removed the document with the field a equal to 3");
+    callback(result);
+  });
+};
+
+// Index a Collection
+// [Indexes](https://docs.mongodb.org/manual/indexes/) can improve your application's
+// performance. The following function creates an index on the **a** field in the
+// **documents** collection.
+const indexCollection = function(db, callback) {
+  db.collection('documents').createIndex(
+    { "a": 1 },
+      null,
+      function(err, results) {
+        console.log(results);
+        callback();
+    }
+  );
+};
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+    insertDocuments(db, function() {
+        indexCollection(db, function() {
+            updateDocument(db, function() {
+                removeDocument(db, function() {
+                    client.close();
+                });
+            });
+        });
+    });
+});
+// END MONGOOSE DB INITIALIZATION
 
 var app = express();
 
