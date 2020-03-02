e0672403d323f37b8719e86679fcb99e7d14ae6c
Cristi DeLeo <cdeleo1@asu.edu>
2020-03-01 23:23:20 -0500

    [03/01/2020] In House Services and sub-pages initial design

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Best Community Service' });
 });
 
+/* GET home page. */
+router.get('/inHouseServices', function(req, res, next) {
+    res.render('inHouseServices', { title: 'In House Services' });
+});
+
+/* GET classes page. */
+router.get('/inHouseServices/classes', function(req, res, next) {
+    res.render('classes', { title: 'Classes' });
+});
 module.exports = router;
