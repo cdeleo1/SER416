c63e2ab0b971064f6c1d8361eaa0aec9eda6c540
Cristi DeLeo <cdeleo1@asu.edu>
2020-03-02 00:33:13 -0500

    [03/01/2020] Initial users.pug edit

--- routes/index.js
+++ routes/index.js
@@ -3,92 +3,110 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Best Community Service' });
+    res.render('index', { title: 'Best Community Service', 
+                          header: 'Welcome!' });
 });
 
 /* GET home page. */
 router.get('/inHouseServices', function(req, res, next) {
-    res.render('inHouseServices', { title: 'In House Services' });
+    res.render('inHouseServices', { title: 'Best Community Service' , 
+                          header: 'In House Services' });
 });
 
 /* GET classes page. */
 router.get('/inHouseServices/classes', function(req, res, next) {
-    res.render('classes', { title: 'Classes' });
+    res.render('classes', { title: 'In House Services' , 
+                          header: 'Classes Offered' });
 });
 
 /* GET classes page. */
 router.get('/inHouseServices/classes/spanishLanguage', function(req, res, next) {
-    res.render('spanishLanguage', { title: 'Class: Spanish Language' });
+    res.render('spanishLanguage', { title: 'Classes Offered' , 
+                          header: 'Spanish Language Class' });
 });
 
 /* GET classes page. */
 router.get('/inHouseServices/classes/artsAndCrafts', function(req, res, next) {
-    res.render('artsAndCrafts', { title: 'Class: Arts and Crafts' });
+    res.render('artsAndCrafts', { title: 'Classes Offered' , 
+                          header: 'Arts and Crafts Class' });
 });
 
 /* GET classes page. */
 router.get('/inHouseServices/classes/painting', function(req, res, next) {
-    res.render('painting', { title: 'Class: Painting' });
+    res.render('painting', { title: 'Classes Offered' , 
+                          header: 'Painting Class' });
 });
 
 /* GET eventRentals page. */
 router.get('/inHouseServices/eventRentals', function(req, res, next) {
-    res.render('eventRentals', { title: 'Event Rentals' });
+    res.render('eventRentals', { title: 'Best Community Service' , 
+                          header: 'Event Rentals' });
 });
 
 /* GET eventRentals page. */
 router.get('/inHouseServices/eventRentals/equipment', function(req, res, next) {
-    res.render('equipment', { title: 'Event Rentals: Equipment' });
+    res.render('equipment', { title: 'Event Rentals' , 
+                          header: 'Equipment Rentals' });
 });
 
 /* GET eventRentals page. */
 router.get('/inHouseServices/eventRentals/venues', function(req, res, next) {
-    res.render('venues', { title: 'Event Rentals: Venues' });
+    res.render('venues', { title: 'Event Rentals' , 
+                          header: 'Venues Available for Rent' });
 });
 
 /* GET externalServices page. */
 router.get('/externalServices', function(req, res, next) {
-    res.render('externalServices', { title: 'External Services' });
+    res.render('externalServices', { title: 'Best Community Service' , 
+                          header: 'External Services' });
 });
 
 /* GET externalServices page. */
 router.get('/externalServices/homeCare', function(req, res, next) {
-    res.render('homeCare', { title: 'External Services: Home Care' });
+    res.render('homeCare', { title: 'External Services' , 
+                          header: 'Home Care' });
 });
 
 /* GET externalServices page. */
 router.get('/externalServices/shuttles', function(req, res, next) {
-    res.render('shuttles', { title: 'External Services: Shuttles' });
+    res.render('shuttles', { title: 'External Services' , 
+                          header: 'Shuttles' });
 });
 
 /* GET volunteering page. */
 router.get('/volunteering', function(req, res, next) {
-    res.render('volunteering', { title: 'Volunteering' });
+    res.render('volunteering', { title: 'Best Community Service' , 
+                          header: 'Volunteering' });
 });
 
 /* GET volunteering page. */
 router.get('/volunteering/currentOpportunities', function(req, res, next) {
-    res.render('currentOpportunities', { title: 'Volunteering: Current Opportunities' });
+    res.render('currentOpportunities', { title: 'Volunteering' , 
+                          header: 'Current Opportunities' });
 });
 
 /* GET volunteering page. */
 router.get('/volunteering/offerAService', function(req, res, next) {
-    res.render('offerAService', { title: 'Volunteering: Offer a Service' });
+    res.render('offerAService', { title: 'Volunteering' , 
+                          header: 'Offer a Service' });
 });
 
 /* GET donations page. */
 router.get('/donations', function(req, res, next) {
-    res.render('donations', { title: 'Donations' });
+    res.render('donations', { title: 'Best Community Service' , 
+                          header: 'Donations' });
 });
 
 /* GET donations page. */
 router.get('/donations/goodsDropOff', function(req, res, next) {
-    res.render('goodsDropOff', { title: 'Donations: Goods Drop-Off' });
+    res.render('goodsDropOff', { title: 'Donations' , 
+                          header: 'Goods Drop-Off' });
 });
 
 /* GET donations page. */
 router.get('/donations/payPal', function(req, res, next) {
-    res.render('payPal', { title: 'Donations: PayPal' });
+    res.render('payPal', { title: 'Donations' , 
+                          header: 'PayPal' });
 });
 
 module.exports = router;
